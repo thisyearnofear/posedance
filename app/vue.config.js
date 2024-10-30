45e30555bb2e58bac7aa792d53f2ce94f96c4fd7
Quincybob <74205675+Quincybob@users.noreply.github.com>
2024-10-30 17:09:38 +0000

    vue config and MIME type

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-	configureWebpack: {
-		devtool: 'source-map',
-	},
-	devServer: {
-		proxy: {
-			'/api': {
-				target: 'http://localhost:7071',
-				ws: true,
-				changeOrigin: true,
-			},
-		},
-	},
+  publicPath: "./",
+  configureWebpack: {
+    devtool: "source-map",
+  },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:7071",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
 };
